77d4c8198c0baeed6252b53317e2ce082e5167ab
Christian <christian.geisler@posteo.de>
2020-02-17 10:23:36 +0100

    remove model loading to test memory limit on heroku

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 from joblib import load
 
-vectorizer = load('fitted_vectorizer.joblib')
-clf = load('fitted_model.joblib')
+# vectorizer = load('fitted_vectorizer.joblib')
+# clf = load('fitted_model.joblib')
 
 app = Flask(__name__, static_folder="./dist/client", static_url_path='')
 
@@ -10,6 +10,6 @@ app = Flask(__name__, static_folder="./dist/client", static_url_path='')
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/api/veggie/predict', methods=['POST'])
-def predict_veggi():
-    return str(clf.predict(vectorizer.transform([request.json.get('phrase')]))[0])
+# @app.route('/api/veggie/predict', methods=['POST'])
+# def predict_veggi():
+#     return str(clf.predict(vectorizer.transform([request.json.get('phrase')]))[0])
