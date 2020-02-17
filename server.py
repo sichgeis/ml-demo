623aeb903de22ad4db8dc0829edbe50678fee2bf
Christian <christian.geisler@posteo.de>
2020-02-17 09:45:25 +0100

    prepare heroku deployment

--- server.py
+++ server.py
@@ -4,8 +4,12 @@ from joblib import load
 vectorizer = load('fitted_vectorizer.joblib')
 clf = load('fitted_model.joblib')
 
-app = Flask(__name__, static_folder="./build", static_url_path='')
+app = Flask(__name__, static_folder="./dist/client", static_url_path='')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def index():
+    return app.send_static_file('index.html')
+
+@app.route('/api/veggie/predict', methods=['POST'])
+def predict_veggi():
     return str(clf.predict(vectorizer.transform([request.json.get('phrase')]))[0])
